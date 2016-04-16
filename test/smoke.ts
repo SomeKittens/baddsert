146b5522734183a04d4202433ba4814dddee0a58
Randall Koutnik <rkoutnik@netflix.com>
2016-04-15 20:04:52 -0700

    chore: added more tests

--- test/smoke.ts
+++ test/smoke.ts
@@ -21,4 +21,33 @@ describe('smoke tests', () => {
     baddsert('some pants', 'llama');
     realB('basic assert', mockStore);
   });
+
+  it('checks against old data and does fine', () => {
+    // This'll throw if something's wrong
+    mockStore.pants = {
+      _meta: {
+        type: 'denim'
+      },
+      reference: 'pants'
+    };
+    baddsert('pants', 'pants');
+  });
+
+  it('should throw if it finds something that does not match', () => {
+    mockStore.pants = {
+      _meta: {
+        type: 'denim'
+      },
+      reference: 'super llama'
+    };
+    let didThrow = true;
+    try {
+      baddsert('pants', 'pants');
+      didThrow = false;
+    } catch (e) {
+      // pants: Expected pants to equal super llama.
+      realB('throw check', e.message);
+    }
+    realB('throw check did throw', didThrow);
+  });
 });
